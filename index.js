da90cf116c9b6f0a33ceacbfc8166fc642de3766
Marina Menezes <marinamenezes@me.com>
2023-10-03 20:37:56 -0300

    integracao back e front

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-const http = require('http');
-const { hostname } = require('os');
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const server = express()
 
-const host = '127.0.0.1';
-const port = 3000;
+server.listen(3001)
+server.use(cors())
+server.use(bodyParser.urlencoded())
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+server.use(express.json())
+const contacts = [
+    {"name": "Marina"},
+    {"name": "João"},
+    {"name": "Maria"}
+]
+
+server.get('/', () => {
+    console.log('executando a rota test com o GET + nodemon atualizando')
 })
 
-server.listen(port, hostname, () => {
-    console.log(`Servidor rodando em http://${host}:${port}/`)
+//Lista
+server.get('/contact', (req, res) => {
+    return res.json(contacts)
+})
+
+//Adiciona
+server.post('/contact', (req, res) => {
+    const {name} = req.body
+    contacts.push({"name": name})
+    return res.json(contacts)
+})
+
+//Atualiza
+server.put('/contact/:index', (req, res) => {
+    const {index} = req.params
+    const {name} = req.body
+
+    contacts[index] = {"name": name}
+
+    return res.json(contacts)
+})
+
+//Deleta
+server.delete('/contact/:index', (req, res) => {
+    const {index} = req.params
+    contacts.splice(index, 1)
+    return res.json(contacts)
 })
